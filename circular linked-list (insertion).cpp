bddc222125af4ebe6b625cceaa14a63f38380508
chirag batheja <99245027+cheenu12@users.noreply.github.com>
2023-01-02 12:20:29 +0530

    Update circular linked-list (insertion).cpp

--- circular linked-list (insertion).cpp
+++ circular linked-list (insertion).cpp
@@ -19,7 +19,7 @@ node(int data)
 void insertnode (node * & tail ,  int element , int datatoinsert)
 {
 
-    if(tail==NULL)
+    if(tail==NULL)                  // if tail is empty we insert the datatoinsert
     {
         cout<< "link-list was empty so i inserted  " << datatoinsert << "  and mark as the tail " <<endl;
         node* newnode = new node(datatoinsert);
@@ -32,12 +32,12 @@ void insertnode (node * & tail ,  int element , int datatoinsert)
     else
     {
         node * curr = tail ;
-        node * curr2 =  tail;
+        node * curr2 =  tail;                           //temparory variable for just for finding if the element from where we want to insert (is exits or not)
         while ( curr->data != element)
         {
 
             curr=curr->next ;
-            if (curr->data == curr2->data)
+            if (curr->data == curr2->data)                  // just for finding if the element from where we want to insert (is exits or not)
             {
                 cout<<"there is no such element exits  " << element <<" " <<endl;
                 cout<<"insert the correct element" <<endl<<endl;
@@ -46,29 +46,29 @@ void insertnode (node * & tail ,  int element , int datatoinsert)
 
         }
 
-        node * newnode = new node(datatoinsert);
+        node * newnode = new node(datatoinsert);   //creating new node when element is found 
              newnode-> next = curr->next;
             curr-> next = newnode;
 
     }
 }
 
-void printnode(node * & tail)
+void printnode(node * & tail)                    // printing function / traversing
 {
 
-if(tail==NULL)
+if(tail==NULL)           // if link-list is empty
 {
 
     cout<<"link-list is empty "<<endl;
     return;
 }
 
-
-node * ttt = tail;
-cout<<tail->data << " ";
+ 
+node * ttt = tail;              //temp variaale for storing data of tail so that we can apply on condition
+cout<<tail->data << " ";        
 tail= tail->next ;
-while(  tail != ttt )
-
+while(  tail != ttt )            //  until when tail reaches the tail again so it will stop.
+ 
 {
     cout<< tail->data << " " ;
    tail=  tail->next;
